126fc848edcc58ceb791f01331ed0b74d0976534
meltombaz <melike.tombaz@gmail.com>
2025-06-13 14:52:45 +0200

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Page setup
-st.set_page_config(page_title="Gene Expression Heatmap", layout="centered")
+st.set_page_config(page_title="Molecule Heatmap", layout="centered")
 
 # Title
 st.title("Heatmap Maker")
@@ -16,14 +16,14 @@ if uploaded_file is not None:
     try:
         df = pd.read_excel(uploaded_file)
 
-        if df.shape[1] < 2 or 'Gene' not in df.columns:
+        if df.shape[1] < 2 or 'Molecule' not in df.columns:
             st.error("The file must have a 'Gene' column and at least one numeric condition column.")
         else:
             # Reshape for seaborn
-            data_long = pd.melt(df, id_vars='Gene', var_name='Condition', value_name='Expression')
+            data_long = pd.melt(df, id_vars='Protein', var_name='Condition', value_name='Average Delta Z-Score')
             # Preserve original Gene order
-            gene_order = df['Gene'].tolist()
-            heatmap_data = data_long.pivot(index='Gene', columns='Condition', values='Expression')
+            gene_order = df['Protein'].tolist()
+            heatmap_data = data_long.pivot(index='Protein', columns='Condition', values='Average Delta Z-Score')
             heatmap_data = heatmap_data.reindex(gene_order)
 
 
@@ -40,14 +40,14 @@ if uploaded_file is not None:
             cmap=sns.diverging_palette(240, 10, as_cmap=True),
             center=0,
             linewidths=0.5,
-            cbar_kws={'label': 'Expression'}
+            cbar_kws={'label': 'Average Delta Z-Score'}
             )
 
             plt.title(custom_title, fontsize=14, fontweight='bold')
             plt.xticks(fontsize=10, fontfamily='sans-serif')
             plt.yticks(fontsize=10, fontfamily='sans-serif', rotation=0)
-            plt.xlabel("Condition", fontsize=12, fontweight='bold', fontfamily='sans-serif')
-            plt.ylabel("Gene", fontsize=12, fontweight='bold', fontfamily='sans-serif')
+            plt.xlabel("", fontsize=12, fontweight='bold', fontfamily='sans-serif')
+            plt.ylabel("", fontsize=12, fontweight='bold', fontfamily='sans-serif')
             plt.tight_layout()
 
             st.pyplot(plt)
